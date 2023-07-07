7e114c8bbdd521e6b6538788eb2e5a080c6dada1
Ricou107 <eric.leroux107@gmail.com>
2023-07-07 11:59:02 +0200

    add build and readme

---
+++ dist/ModalComponent.js
@@ -0,0 +1,25 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = ModalComponent;
+var _react = _interopRequireDefault(require("react"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function ModalComponent(_ref) {
+  let {
+    children
+  } = _ref;
+  const style = {
+    position: 'absolute',
+    zIndex: '2',
+    backgroundColor: 'grey',
+    width: '80%',
+    margin: '0 calc(100% - 90%)',
+    top: '15%',
+    borderRadius: '10px'
+  };
+  return /*#__PURE__*/_react.default.createElement(_react.default.Fragment, null, /*#__PURE__*/_react.default.createElement("section", {
+    style: style
+  }, children));
+}
--- src/lib/ModalComponent.jsx
+++ src/lib/ModalComponent.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export default function ModalComponent({children}) {
     const style = {
         position: 'absolute',
